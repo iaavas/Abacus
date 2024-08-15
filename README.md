db7b9de0c2fc6586ba370a3d9a27a6ea9815d6f3
iaavas <baralaavas@gmail.com>
2024-08-15 09:40:43 +0545

    Setup for project and base layer created

---
+++ README.md
@@ -0,0 +1,15 @@
+# Neural Network from Scratch
+
+This is a project to implement neural network from scratch, only using the pandas library for mathematical purposes.
+
+## How to Run It?
+
+### Install the requirements
+
+```python3
+pip install -r requirements.txt
+```
+
+### Currently under development
+
+It will be available soon.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neural_network',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A neural network package from scratch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Aavash Baral',
+    author_email='baralaavas@gmail.com',
+    url='https://github.com/iaavas/neural_network_from_scratch',
+    install_requires=[],
+)
